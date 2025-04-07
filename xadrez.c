dbb53b084c4d3f402933c1338fb899869464c6f0
LuizHenrique-webdev <almeidaluiz578@gmail.com>
2025-04-07 18:19:35 +0000

    implementei recursividade e loops anhinhados - nivel mestre

--- xadrez.c
+++ xadrez.c
@@ -4,44 +4,67 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+void MoverTorre(int i){
+    if(i>0){
+        printf("Direita\n");
+        MoverTorre(i-1);
+    }
+}
+
+void MoverBispo(int j){
+    if (j>0){
+        
+        for (int k=0; k<1; k++){
+            printf("Cima ");
+            for (int l=0; l<1; l++){
+                printf("Direita\n");
+            }
+            
+        } 
+        MoverBispo(j-1);
+    }
+    
+}
+
+void MoverRainha(int k){
+    if(k>0){
+        printf("esquerda\n");
+        MoverRainha(k-1);
+    }
+}
+
 int main() {
-    int j=0, k=0, L=0;                       //declaração de variavel iniciaaando em 0
+    int Torre=8, Bispo=5, Rainha=8;      //declaração de variavel
 
     printf("JOGO DE XADREZ\n");          //cabeçalho
     printf("    \n");                    //linha em branco/vazio (estetica)
     printf("Torre\n");                   //Torre,  informa os movimentos da torre
 
-    for (int i=0; i<5; i++){
-        printf("direita\n");             //escreve direita 5 vezes, ccomeçando a contar a partir do zero //i++ incrementa +1 a  cada loop
-    }
+    MoverTorre(Torre);                   //procedimento/função é chamada para executar os  movimentos da  Torre
+
     printf("  \n");
     printf ("Bispo\n");                  //Bispo,  informa os movimentos do Bispo
 
-    while(j<5){
-        printf("cima-direita\n");       //escreve cima-direita 5 vezes, ccomeçando a contar a partir do zero
-        j++;                            //incrementa +1 a cada loop
-    }
+    MoverBispo(Bispo);                   //procedimento/função é chamada para executar os  movimentos do bispo
 
     printf("  \n");
     printf ("Rainha\n");                 //Rainha,  informa os movimentos da Rainha
 
-    do{
-        printf("esquerda\n");            //escreve esquerda 5 vezes, começando a contar a partir do zero
-        k++;                             //incrementa +1 a cada loop
-    }while (k<8);
+    MoverRainha(Rainha);                 //procedimento/função é chamada para executar os  movimentos da  Rainha
 
     printf(" \n");
     printf ("Cavalo\n");
     
-    while(L==0){                         //while  vai repetir enquanto a variavel tiver velor zero //variavel inicialmente começa com valor zero
-        printf ("Baixo\n");              //primeiro loop faz os 2 primeiros movimentos
-        printf ("Baixo\n");
-        for (int G=1; G<2; G++){         //inicia segundo loop
-            printf("esquerda\n");        //prime o segundo movimento
-        }
-        L++;                             //incrementa 1 para a variavel,  que deixa de ser zero, encerrando o loop
+    for (int L=0,  M=0; L<1, M<1; L++, M++){    //repetição complexa com duas variaveis 
+        printf("cima\n");                       // cavalo sempre terá dois movimetos verticais iguais (cima-cima ou baixo-baixo) //para o exigido era cima-cima
+        printf("cima\n");
+        if((L && M) >=0){                       //se os dois movimentos verticais forem executados, inicia o movimento horizontal 
+            for (int N=0; N<1; N++){            //repetição aninhada para movimento horizointal 
+                printf("Direita\n");            //um movimento para horizontal (exigido direita)
+            }
+        }continue;                              //controla o loop condicional
     }
 
-                                         //para os movimentos do cavalo poderia ser usado a estrutura do/while para a mesma logica, no entretanto usei while apenas para treinar novas formas
+                                         
     return 0;
 }
